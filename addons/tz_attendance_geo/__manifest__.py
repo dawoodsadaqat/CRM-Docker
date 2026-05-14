ce080af7b7ef8a389763b6526dd081adbaf470bd
Dawood bin Sadaqat <dawoodsadaqat@gmail.com>
2026-05-15 00:14:13 +0400

    attendance custom module

---
+++ addons/tz_attendance_geo/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "TZ Attendance Geo",
+    "version": "17.0.1.0.0",
+    "summary": "GPS check-in/check-out and fake attendance detection",
+    "category": "Human Resources",
+    "author": "Techzilla",
+    "license": "LGPL-3",
+    "depends": [
+        "hr",
+        "hr_attendance",
+        "tz_live_tracking",
+        "tz_hr_extension",
+    ],
+    "data": [
+        "views/hr_attendance_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
