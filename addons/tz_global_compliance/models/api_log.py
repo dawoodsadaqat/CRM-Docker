a2605acca0a128efddad983c4607165cc8f03a83
Dawood bin Sadaqat <dawoodsadaqat@gmail.com>
2026-05-15 19:49:13 +0400

    update

--- addons/tz_global_compliance/models/api_log.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class TzApiLog(models.Model):
-    _inherit = "tz.api.log"
-
-    tz_compliance_failed = fields.Boolean(string="Compliance Failed", default=False)
